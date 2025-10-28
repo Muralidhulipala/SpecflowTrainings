dee527f433fc3ccad7a7e9cd35ee87e654c778d1
Admin <Admin@DESKTOP-T83UQOC>
2025-10-28 20:36:49 +0530

    Add project files.

---
+++ SeleniumExamples/Program.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            //Text Field
+            driver.FindElement(By.XPath("")).SendKeys("");
+            driver.FindElement(By.XPath("")).SendKeys("");
+            //Radio button
+            driver.FindElement(By.XPath("")).Click();
+            driver.FindElement(By.XPath("")).Click();
+            //Checkbox
+            driver.FindElement(By.XPath("")).Click();
+            driver.FindElement(By.XPath("")).Click();
+
+            //Dropdowns
+
+            SelectElement select = new SelectElement(driver.FindElement(By.XPath("")));
+           var list= select.AllSelectedOptions;
+            select.SelectByValue("");
+            select.SelectByText("");
+            select.SelectByIndex(1);
+            var selectedvalue=select.SelectedOption.Text;
+
+            int totaloptions=select.Options.Count;
+
+
+            //Alerts
+            IAlert alert = driver.SwitchTo().Alert();
+            var text=alert.Text;
+            alert.Accept();
+            alert.Dismiss();
+            alert.SendKeys("");
+
+            //Frames
+            driver.SwitchTo().Frame(1);
+            driver.FindElement(By.XPath("")).Click();
+            driver.FindElement(By.XPath("")).Click();
+            driver.SwitchTo().DefaultContent();
+
+            //windows
+
+            var wincount = driver.WindowHandles;
+            foreach (var win in wincount)
+            {
+                driver.SwitchTo().Window(win);
+               var title= driver.Title;
+
+            }
+
+            // Implict wait
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Webdriver wait explicit wait
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("")));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
